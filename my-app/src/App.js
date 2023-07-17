27d634ed112d26bb815ec259eacb91a0cda15b2f
Sepehr Zohoori Rad <81197023+Sepzie@users.noreply.github.com>
2023-07-17 14:44:54 -0700

    Created basic card and list

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,84 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+
+function VehicleCard({ vehicle, setSelectedVehicle }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vehicle-card">
+      <h3>{vehicle.make} {vehicle.model}</h3>
+      <p>Year: {vehicle.year}</p>
+      <p>Price: ${vehicle.price}</p>
+      <button onClick={() => setSelectedVehicle(vehicle)}>Select</button>
     </div>
   );
 }
+
+function VehicleList({ vehicles, setSelectedVehicle }) {
+  return (
+    <div className="vehicle-list">
+      {vehicles.map((vehicle, index) => (
+        <VehicleCard key={index} vehicle={vehicle} setSelectedVehicle={setSelectedVehicle} />
+      ))}
+    </div>
+  );
+}
+
+function VehicleDetails({ vehicle, unselectVehicle }) {
+  return (
+    <div className="vehicle-card">
+      <h3>{vehicle.make} {vehicle.model}</h3>
+      <p>Year: {vehicle.year}</p>
+      <p>Price: ${vehicle.price}</p>
+      <button onClick={unselectVehicle}>back</button>
+    </div>
+  );
+}
+
+
+function App() {
+  var [selectedVehicle, setSelectedVehicle] = useState(null);
+
+  return (
+    <div className="App">
+      {selectedVehicle && <VehicleDetails vehicle={selectedVehicle} unselectVehicle={() => setSelectedVehicle(null)} />}
+      {!selectedVehicle && <VehicleList vehicles={mockVehicles} setSelectedVehicle={setSelectedVehicle} />}
+    </div>
+  );
+}
+
+
+const mockVehicles = [
+  {
+    make: 'Ford',
+    model: 'Mustang',
+    year: 2023,
+    price: 35000
+  },
+  {
+    make: 'Chevrolet',
+    model: 'Camaro',
+    year: 2023,
+    price: 37000
+  },
+  {
+    make: 'Dodge',
+    model: 'Charger',
+    year: 2023,
+    price: 40000
+  },
+  {
+    make: 'Tesla',
+    model: 'Model S',
+    year: 2023,
+    price: 80000
+  },
+  {
+    make: 'Honda',
+    model: 'Civic',
+    year: 2023,
+    price: 25000
+  },
+];
 
 export default App;
